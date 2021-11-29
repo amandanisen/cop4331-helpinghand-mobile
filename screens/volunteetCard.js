e038ede10732f871ad00c53b670026ba5c02b4e9
harrismichael2095 <45339433+harrismichael2095@users.noreply.github.com>
2021-11-29 00:48:01 -0500

    close task button now works

--- screens/volunteetCard.js
+++ screens/volunteetCard.js
@@ -9,30 +9,67 @@ import {
 } from "react-native";
 
 import { Card, ListItem, Icon, Button } from "react-native-elements";
+var user_data= JSON.parse(localStorage.getItem("user_data"));
+
 
 export default function CoordCard(props) {
   function closeTask() {
-    //make api call to close task
-    //pass task id i assume
-    console.log("clicked close task");
-  }
+    async function close(){
+      console.log(user_data.email)
+      var obj = {email:user_data.email,taskID:props.id};
+      var js=JSON.stringify(obj);
+      console.log(js);
+
+      if (window.confirm("Are you sure you want to remove this task?")) {
+      try{
+        const response = await fetch("https://helpinghand-cop4331.herokuapp.com/vol/removeTask",{
+          method: "POST",
+          headers: {"Content-Type" : "application/json"},
+          body : js,
+        });
+        var res=JSON.parse(await response.text() );
+        if (res.success != true){
+          console.log(res.error);
+        } else{
+          console.log(res.success);
+          console.log("success");
+          
+          if(res != "nos such user found"){
+            setPosts(res);
+          } else{
+            console.log("The call might have failed above buts its okay, there were no tasks");
+          }
+          return res;
+        }
+      } catch (e){
+        console.log((e.toString()));
+        return ;
+      }
+      
+    }
+    
+    }
+      //test
+    close();
+     ;
+  //make api call to close task
+  //pass task id i assume
+  console.log("clicked close task");
+}
+
+
 
   return (
     <Card borderRadius={5}>
       <Card.Title style={{ fontSize: 20, marginBottom: 2 }}>
-        {" "}
         {props.name}
       </Card.Title>
       <Card.Divider />
       <Text style={{ marginBottom: 10, color: "#808080" }}>
-        {props.numVol + " Slots Left " +" --- " + props.maxVol + " Total Slots"}
+        {"Date: "+props.task_date+"       "+ props.address + "       Volunteers Needed: "+ props.numVol }
       </Text>
+      
       <Text style={{ marginBottom: 10 }}>{props.description}</Text>
-      <Text style={{ marginBottom: 10, color: "#808080" }}>
-        {"Coordinates:  " +props.task_location[0] + "     " + props.task_location[1]}
-      </Text>
-      <Text style={{ marginBottom: 10 }}>{props.task_date}</Text>
-
       <Button
         icon={<Icon name="check" color="#ffffff" />}
         onPress={() => closeTask()}
@@ -43,7 +80,7 @@ export default function CoordCard(props) {
           marginBottom: 0,
           backgroundColor: "#013d38",
         }}
-        title="Close Task"
+        title="Done"
       />
     </Card>
   );
